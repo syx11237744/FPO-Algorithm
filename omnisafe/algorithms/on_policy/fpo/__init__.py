3bdf97fe974305d9b42fa88c8c2d5aa32e49d6a1
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2025-04-22 16:00:45 +0800

    update FPONG

--- omnisafe/algorithms/on_policy/fpo/__init__.py
+++ omnisafe/algorithms/on_policy/fpo/__init__.py
@@ -1,7 +1,7 @@
 """FPO algorithms."""
 
 from omnisafe.algorithms.on_policy.fpo.fpo import FPO
-from omnisafe.algorithms.on_policy.fpo.fpo_NaturalPG import FPONG
+from omnisafe.algorithms.on_policy.fpo.fpo_ng import FPONG
 
 __all__ = [
     'FPO',
