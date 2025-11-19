a2a549b1ee05f27074a780259c13be475b663ff6
yangyujie <yangyj21@mails.tsinghua.edu.cn>
2025-11-19 21:43:58 +0800

    add safety hopper env

--- omnisafe/envs/safety_gymnasium_env.py
+++ omnisafe/envs/safety_gymnasium_env.py
@@ -121,6 +121,7 @@ class SafetyGymnasiumEnv(CMDP):
         'SafetyHumanoidVelocity-v1',
         'SafetyPointRun0-v0',
         'SafetyCarRun0-v0',
+        'SafetyHopper-v4',
     ]
 
     def __init__(
